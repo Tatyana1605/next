0a5cbbec759779c2a07172fd4582e2d3bcca58cb
Tatyana1605 <tiponomareva88@gmail.com>
2018-10-01 21:45:02 +0700

    next9

--- src/assets/scripts/about.js
+++ src/assets/scripts/about.js
@@ -1,1 +1,17 @@
 import "./modules/example";
+
+// $(document).ready(function(){
+//   $('.btn__nav').on('click', function(e){
+//     e.preventDefault();
+//     $('.nav').addClass('nav__activ');
+//   });
+
+//   $('.nav__closes').on('click', function(e){
+//     e.preventDefault();
+//     $('.nav').removeClass('nav__activ');
+//   });
+
+//   $('.nav__item').on('click', function(e){
+//     $('.nav').removeClass('nav__activ');
+//   })
+// });
